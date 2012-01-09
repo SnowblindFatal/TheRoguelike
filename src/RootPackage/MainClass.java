f1344ca1eb3e9955a819b4454a5d8cd5a704d4ad
SnowblindFatal <fatality@gmail.com>
2012-01-09 06:50:14 +0200

    First commit. Should be good!

---
+++ src/RootPackage/MainClass.java
@@ -0,0 +1,18 @@
+package RootPackage;
+import LevelGenerators.StupidGenerator;
+
+/**
+ * User: JS
+ * Date: 9.1.2012
+ * Time: 6:19
+ * Package: LevelGenerators
+ */
+
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("helo");
+        StupidGenerator newMap = new StupidGenerator();
+    }
+}
